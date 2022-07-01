8f39c7e935a5f8737b04c7a60d7ba8e1d6b8f49a
ChefKhoiRD <vu.khoi6111@gmail.com>
2022-07-01 14:19:11 -0600

    removed 404.html, successfully connected get route to notes.html

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require ('fs');
-const db = require('./miniature-eureka/Develop/db/db.json');
+const db = require('./db.json');
 
 // decalring app for express and giving it a port of 3001
 const PORT = process.env.PORT || 3001;
@@ -17,17 +17,12 @@ app.use(express.static('public'));
 
 // get route to main page
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/miniature-eureka/Develop/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // get route to note page
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/miniature-eureka/Develop/public/notes.html'))
-);
-
-// get route to 404 error page
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/miniature-eureka/Develop/public/404.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // set app.listen to port declared at 3001
